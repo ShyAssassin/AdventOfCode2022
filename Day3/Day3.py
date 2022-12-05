63aba912bb7a354108e5559b63eba66d45048c9e
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2022-12-05 11:08:40 +0200

    Added Day3

---
+++ Day3/Day3.py
@@ -0,0 +1,30 @@
+def CalculateScore(c: str) -> int:
+    if c.isupper():
+        return int(ord(c) - ord('A') + 27)
+    else:
+        return int(ord(c) - ord('a') + 1)
+
+Score1 = 0
+with open("Input.txt", "r") as file:
+    for line in file:
+        line = line.strip()
+        FirstHalf, SecondHalf = line[:len(line)//2], line[len(line)//2:]
+        for letter in FirstHalf:
+            if letter in SecondHalf:
+                Score1 += CalculateScore(letter)
+                break
+
+
+Score2 = 0
+with open("Input.txt", "r") as file:
+    lines = file.readlines()
+    i = 0
+    while i < len(lines):
+        for letter in lines[i]:
+            if letter in lines[i+1] and letter in lines[i+2]:
+                Score2 += CalculateScore(letter)
+                break
+        i += 3
+
+print("Part 1:", Score1)
+print("Part 2:", Score2)
