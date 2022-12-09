f7df865a4d630e846430a3ba7827f5f0e757ef8c
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2022-12-09 16:55:51 +0200

    Added Day6

---
+++ Day6/Day6.py
@@ -0,0 +1,12 @@
+with open("Input.txt") as file:
+    data = file.read()
+
+def solve(data, n=4):
+    # some cursed shit right here
+    groups = [(data[i:i+n]) for i in range(0, len(data)-n+1)]
+    for i, group in enumerate(groups):
+        if len(set(group)) == len(group):
+            return i + n
+            
+print(f"part 1: {solve(data, n=4)}")
+print(f"part 2: {solve(data, n=14)}")
